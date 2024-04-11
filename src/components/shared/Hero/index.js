e8c380549d94cc338d505dce0ee899402645d43c
chrod <chrod.dev@gmail.com>
2024-04-11 18:39:30 -0500

    Add fade effect to Hero slides

--- src/components/shared/Hero/index.js
+++ src/components/shared/Hero/index.js
@@ -1,20 +1,22 @@
 'use client';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
 import Image from 'next/image';
+import { Autoplay, EffectFade } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
 import styles from './Hero.module.scss';
-import { Autoplay } from 'swiper/modules';
 
 export const Hero = () => {
 	return (
 		<section className={styles.Hero}>
 			<Swiper
-				modules={[Autoplay]}
+				modules={[Autoplay, EffectFade]}
 				slidesPerView={1}
 				loop={true}
 				autoplay={{
 					delay: 5000,
 				}}
+				effect={'fade'}
 			>
 				{[1, 2, 3, 4].map(slide => (
 					<SwiperSlide key={`slide-${slide}`}>
