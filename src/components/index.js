e4c39738f279bf6ce71d1ef7848d8f7792d22f2a
chrod <chrod.dev@gmail.com>
2024-04-16 18:07:02 -0500

    Update some styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 export { Header } from './shared/Header';
 export { Menu } from './shared/Menu';
 export { Hero } from './shared/Hero';
+export { ContactForm } from './shared/ContactForm';
+
 export { Overview } from './home/Overview';
 export { MainProducts } from './home/MainProducts';
 export { Branches } from './home/Branches';
 export { Contact } from './home/Contact';
-export { ContactForm } from './shared/ContactForm';
