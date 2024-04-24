9537ac4bf34ebd1032739f11b87356e9439cccd0
chrod <chrod.dev@gmail.com>
2024-04-24 16:37:08 -0500

    Add services to get categories and products

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,39 @@
-import { getDocs, query } from 'firebase/firestore';
+import { db } from '@/firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 export const fetchData = async ref => {
 	const q = query(ref);
+	try {
+		const querySnapshot = await getDocs(q);
+		const array = [];
+		querySnapshot.forEach(doc => {
+			array.push(doc.data());
+		});
+		return array;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const getCategories = async () => {
+	const q = query(collection(db, 'product_cats'));
+	try {
+		const querySnapshot = await getDocs(q);
+		const array = [];
+		querySnapshot.forEach(doc => {
+			array.push(doc.data());
+		});
+		return array;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const getProducts = async categoryId => {
+	const q = query(
+		collection(db, 'products'),
+		where('product_cat', 'array-contains', categoryId),
+	);
 	try {
 		const querySnapshot = await getDocs(q);
 		const array = [];
